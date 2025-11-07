461f969a1780d35773451ffdb34c69cadeb7102e
Luizabarbosa6 <153461981+Luizabarbosa6@users.noreply.github.com>
2025-11-07 00:13:51 -0300

    Correção da entidade proposta

--- src/main/java/br/com/finexus/crowdfunding/model/Usuario.java
+++ src/main/java/br/com/finexus/crowdfunding/model/Usuario.java
@@ -1,9 +1,13 @@
 package br.com.finexus.crowdfunding.model;
 
+import java.util.ArrayList;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "usuarios")
@@ -18,38 +22,73 @@ public class Usuario {
 
     @NotBlank
     @Email
-    @Column(nullable =  false, unique = true) //indica que email tem que ser unico
+    @Column(nullable = false, unique = true) // indica que email tem que ser unico
     private String email;
 
     @NotBlank
-    private  String senha;
+    private String senha;
 
-    @Enumerated(EnumType.STRING) 
+    @Enumerated(EnumType.STRING)
     private TipoUsuario tipo;
 
     @Column(length = 14, unique = true)
     private String cpf;
 
+    @OneToMany(mappedBy = "solicitante", cascade = CascadeType.ALL)
+    @JsonManagedReference("usuario-proposta")
+    private List<Proposta> propostas = new ArrayList<>();
+
+    @OneToOne(mappedBy = "usuario", cascade = CascadeType.ALL)
+    @JsonManagedReference("usuario-formulario")
+    private FormularioRisco formularioRisco;
 
     // getters e setters
-    public Long getId () {return id;}
-    public void setId (Long id){this.id= id;}
+    public Long getId() {
+        return id;
+    }
 
-    public  String getNome() {return nome;}
-    public void setNome (String nome) {this.nome = nome;}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getEmail () {return email;}
-    public void setEmail(String email){ this.email= email;}
+    public String getNome() {
+        return nome;
+    }
 
-    public String getSenha (){return senha;}
-    public void setSenha (String senha) { this.senha = senha;}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-    public String getCpf (){return cpf;}
-    public void setCpf(String cpf) {this.cpf = cpf;}
+    public String getEmail() {
+        return email;
+    }
 
-    public TipoUsuario getTipo(){return tipo;}
-    public void setTipo ( TipoUsuario tipo){ this.tipo = tipo;}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
+    public String getSenha() {
+        return senha;
+    }
 
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public TipoUsuario getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoUsuario tipo) {
+        this.tipo = tipo;
+    }
 
 }
